ea5903b1476ca358a6b20b9e981fa6573fc82a96
Ankush Gupta <ankushgupta9675@gmail.com>
2024-12-09 12:13:27 +0530

    all database change done

--- Models/qreryInterface.js
+++ Models/qreryInterface.js
@@ -21,47 +21,7 @@ exports.changeInData = async (queryInterface) => {
     //   .catch((err) => {
     //     console.log(err);
     //   });
-    queryInterface
-      .addColumn("users", "totalExperienceInYears", {
-        type: DataTypes.INTEGER,
-      })
-      .then((res) => {
-        console.log("added!");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    queryInterface
-      .addColumn("instructorHistorys", "totalExperienceInYears", {
-        type: DataTypes.INTEGER,
-      })
-      .then((res) => {
-        console.log("added!");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
 
-    // await queryInterface.dropTable("user_ht_slotes");
-    // await queryInterface.dropTable("hTBookings");
-    // await queryInterface.dropTable("homeTutorHistories");
-    // console.log("homeTutorHistories");
-    // await queryInterface.dropTable("ht_coupons");
-    // console.log("ht_coupons");
-    // await queryInterface.dropTable("hTutorImages");
-    // console.log("hTutorImages");
-    // await queryInterface.dropTable("hTPayments");
-    // console.log("hTPayments");
-    // await queryInterface.dropTable("hTTimeSlots");
-    // console.log("hTTimeSlots");
-    // await queryInterface.dropTable("hTPrices");
-    // console.log("hTPrices");
-    // await queryInterface.dropTable("homeTutorReviews");
-    // console.log("homeTutorReviews");
-    // await queryInterface.dropTable("hTServiceAreas");
-    // console.log("hTServiceAreas");
-    // await queryInterface.dropTable("homeTutors");
-    // console.log("homeTutors");
   } catch {
     (err) => {
       throw new Error(err.message);
